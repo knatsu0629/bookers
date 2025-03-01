c81af23a7b596b196c088b6e58ea73c05750e218
knatsu0629 <196932674+knatsu0629@users.noreply.github.com>
2025-03-01 14:57:28 +0000

    [Finish] バリデーションの設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,40 @@
 class BooksController < ApplicationController
 
   def create
-   book = Book.(book_params)
-   book.save
-   redirect_to books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all  
+      render :index
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
-  end
-
+   @book = Book.find(params[:id])
+   end
+ 
   def edit
-
+    @book = Book.find(params[:id])
   end
 
   def update
-
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+    
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
@@ -29,5 +42,4 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
